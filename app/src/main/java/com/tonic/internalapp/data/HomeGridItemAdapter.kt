d04ccaffa0798b7b2c06e8638c00c06d00914444
richie.shih <kotzen1023@gmail.com>
2021-10-08 13:42:21 +0800

    richie commit

--- app/src/main/java/com/tonic/internalapp/data/HomeGridItemAdapter.kt
+++ app/src/main/java/com/tonic/internalapp/data/HomeGridItemAdapter.kt
@@ -1,13 +1,23 @@
 package com.tonic.internalapp.data
 
 import android.content.Context
+import android.graphics.Color
+import android.graphics.Typeface.BOLD
+import android.icu.text.Transliterator
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebSettings
 import android.widget.ArrayAdapter
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.app.NotificationCompat
 import com.tonic.internalapp.R
+import org.w3c.dom.Text
+import ru.nikartm.support.BadgePosition
+import ru.nikartm.support.ImageBadgeView
+import ru.nikartm.support.model.Badge
+import java.time.format.TextStyle
 
 class HomeGridItemAdapter(context: Context?, resource: Int, objects: ArrayList<HomeGridItem>) :
     ArrayAdapter<HomeGridItem>(context as Context, resource, objects) {
@@ -56,6 +66,24 @@ class HomeGridItemAdapter(context: Context?, resource: Int, objects: ArrayList<H
         val homeGridItem = items[position]
         //if (receiptDetailItem != null) {
         holder.icon.setImageResource(homeGridItem.getImgId())
+        if (position == 0) {
+            holder.icon.badgeValue = 27
+            holder.icon.badgePosition = BadgePosition.BOTTOM_RIGHT
+            holder.icon.badgeTextSize = 24f
+            holder.icon.setBadgePadding(10)
+        }
+        //holder.icon.badgeTextSize = 12F
+        //app:ibv_badgeTextSize="12sp"
+        //holder.icon.setFixedBadgeRadius(15f)
+        //app:ibv_fixedBadgeRadius="15dp"
+        //holder.icon.badgeTextStyle = BOLD
+        //app:ibv_badgeTextStyle="bold"
+        //holder.icon.badgeTextColor = Color.rgb(0xff, 0xff, 0xff)
+        //app:ibv_badgeTextColor="#ffffff"
+        //holder.icon.badgeColor = Color.rgb(0x00, 0xac, 0xc1)
+        //app:ibv_badgeColor="#00ACC1"
+        //holder.icon.setLimitBadgeValue(false)
+        //app:ibv_badgeLimitValue="false"
         holder.header.setText(homeGridItem.getStringId())
 
 
@@ -64,7 +92,7 @@ class HomeGridItemAdapter(context: Context?, resource: Int, objects: ArrayList<H
     }
 
     class ViewHolder (view: View) {
-        var icon: ImageView = view.findViewById(R.id.imageViewHomeGrid)
+        var icon: ImageBadgeView = view.findViewById(R.id.imageViewHomeGrid)
         var header: TextView = view.findViewById(R.id.textViewHomeGrid)
     }
 }
